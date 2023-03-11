0a888dcd93f5868bd18458ffe3d3218daa7b4bfc
blucin <glaivesharp@gmail.com>
2023-03-11 23:13:42 +0530

    Edit the default homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../global.css'
+</script>
+
+<h1>SGPA Calculator</h1>
+<p>Subject Count: </p>
