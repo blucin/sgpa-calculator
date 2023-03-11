70095b7dfffbf0b25c6954e92f8e2224f0fa78c1
blucin <glaivesharp@gmail.com>
2023-03-12 01:32:36 +0530

    Finalize form handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,91 @@
 <script lang="ts">
-    import '../global.css';
-    import { onMount } from 'svelte';
+  import "../global.css";
+  import { onMount } from "svelte";
+  import { writable } from "svelte/store";
 
-    // set subCnt to 0 or the value in localStorage
-    let subCnt:number = getSubCnt();
+  type formDataType = {
+    subjectName: string;
+    grade: number;
+    credits: number;
+  }[];
 
-    function getSubCnt():number {
-        if(typeof localStorage !== 'undefined') {
-            return parseInt(localStorage.getItem('subCnt') || '0');
-        }
-        return 0;
+  // set subCnt to 0 or the value in localStorage
+  let subCnt: number = getSubCnt();
+  const formData = writable([
+    {
+      subjectName: "",
+      grade: 0,
+      credits: 0,
+    },
+  ]);
+
+  function getSubCnt(): number {
+    if (typeof localStorage !== "undefined") {
+      return parseInt(localStorage.getItem("subCnt") || "0");
     }
+    return 0;
+  }
 
-    const updateSubCnt = (newValue:number) => {
-        if(typeof localStorage !== 'undefined') {
-            localStorage.setItem('subCnt', newValue.toString());
-        }
-        subCnt = newValue;
-    }   
-
-    const handleIncrementClick = () => {
-        updateSubCnt(subCnt+1);
+  const updateSubCnt = (newValue: number) => {
+    if (typeof localStorage !== "undefined") {
+      localStorage.setItem("subCnt", newValue.toString());
     }
+    subCnt = newValue;
+  };
 
-    const handleDecrementClick = () => {
-        if(subCnt==0) 
-            return;
-        updateSubCnt(subCnt-1);
-    }
+  const handleIncrementClick = () => {
+    formData.update((value) => [...value, { subjectName: "", grade: 0, credits: 0 }]);
+    updateSubCnt(subCnt + 1);
+  };
 
+  const handleDecrementClick = () => {
+    if (subCnt == 0) return;
+    formData.update((value) => value.slice(0, value.length - 1));
+    updateSubCnt(subCnt - 1);
+  };
+
+  /*
     onMount(() => {
-        type formDataType = {
-            SubjectName: string,
-            Grade: number,
-            Credits: number
-        }[];
-
-        // check for old form data
-        let formData:formDataType = getFormData(); 
-
-        function getFormData():formDataType {
-            if(typeof localStorage !== 'undefined') {
-                return JSON.parse(localStorage.getItem('formData') || '[]');
-            }
-            return [];
+        // set formData to the old value in localStorage
+        if(typeof localStorage !== 'undefined') {
+            const oldFormData = JSON.parse(localStorage.getItem('formData') || '[]') as formDataType;
+            formData = oldFormData;
+            updateSubCnt(oldFormData.length);
         }
     });
+    */
 </script>
 
-<h1>Test</h1>
+<h1>SGPA Calculator</h1>
 
 <div class="incr-btn-container">
-    <button class="btn" on:click={handleDecrementClick}> - </button>
-    <p>Subject Count: {subCnt}</p>
-    <button class="btn" on:click={handleIncrementClick}> + </button>
+  <button class="btn" on:click={handleDecrementClick}> - </button>
+  <p>Subject Count: {subCnt}</p>
+  <button class="btn" on:click={handleIncrementClick}> + </button>
 
-    {#if subCnt>0}
-        <div class="form">
-            <input type="text" placeholder="Subject Name" />
-            <input type="number" placeholder="Grade" min="0" />
-            <input type="number" placeholder="Credits" min="0"/>
-        </div>
-    {/if}
+  {#if subCnt > 0}
+    <div class="form">
+      <ul>
+        {#each $formData as data}
+          <input
+            type="text"
+            placeholder="Subject Name"
+            bind:value={data.subjectName}
+          />
+          <input
+            type="number"
+            placeholder="Grade"
+            min="0"
+            bind:value={data.grade}
+          />
+          <input
+            type="number"
+            placeholder="Credits"
+            min="0"
+            bind:value={data.credits}
+          />
+        {/each}
+      </ul>
+    </div>
+  {/if}
 </div>
-
